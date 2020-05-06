5d716d1947d77367770e7676aee24ab1e9ea40cb
Natalia Chelmecki <natalia.chelmecki@mail.utoronto.ca>
2020-05-05 22:34:28 -0400

    Removed base code, added in class for grapher and class for starting page of application.

--- tkinterGUI.py
+++ tkinterGUI.py
@@ -1,5 +1,44 @@
-import tkinter
+import tkinter as tk
 
 
-top = tkinter.Tk()
-top.mainloop()
+LARGE_FONT = ("Verdana, 12")
+
+#main class for grapher application
+class grapher(tk.Tk):
+
+    def __init__(self, *args, **kwargs):
+        tk.Tk.__init__(self, *args, **kwargs)
+        container = tk.Frame(self)
+
+        container.pack(side="top", fill="both", expand = True)
+
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
+
+        self.frames = {}
+
+        frame = startPage(container, self)
+
+        self.frames[startPage] = frame
+
+        #stretches from center to all sides
+        frame.grid(row=0, column=0, sticky="nsew")
+
+        self.show_frame(startPage)
+
+    def show_frame(self, cont):
+        frame = self.frames[cont]
+        frame.tkraise()
+
+
+class startPage(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        label = tk.Label(self, text="Start Page", font=LARGE_FONT)
+        label.pack(padx=10,pady=10)
+
+
+app = grapher()
+app.mainloop()
+
+
