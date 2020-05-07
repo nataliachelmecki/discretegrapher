b261241b3102d4706f5a100f6624fad7410eef5b
Natalia Chelmecki <natalia.chelmecki@mail.utoronto.ca>
2020-05-07 19:30:07 -0400

    Fixed error with showing multiple changes

--- tkinterGUI.py
+++ tkinterGUI.py
@@ -1,28 +1,29 @@
 import tkinter as tk
 
-
 LARGE_FONT = ("Verdana, 12")
 
-#main class for grapher application
-class grapher(tk.Tk):
+
+# main class for grapher application
+class Grapher(tk.Tk):
 
     def __init__(self, *args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
         container = tk.Frame(self)
 
-        container.pack(side="top", fill="both", expand = True)
+        container.pack(side="top", fill="both", expand=True)
 
         container.grid_rowconfigure(0, weight=1)
         container.grid_columnconfigure(0, weight=1)
 
         self.frames = {}
 
-        frame = startPage(container, self)
+        for F in (startPage, pageOne):
+            frame = F(container, self)
 
-        self.frames[startPage] = frame
+            self.frames[F] = frame
 
-        #stretches from center to all sides
-        frame.grid(row=0, column=0, sticky="nsew")
+            """stretches from center to all sides"""
+            frame.grid(row=0, column=0, sticky="nsew")
 
         self.show_frame(startPage)
 
@@ -35,10 +36,24 @@ class startPage(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
         label = tk.Label(self, text="Start Page", font=LARGE_FONT)
-        label.pack(padx=10,pady=10)
+        label.pack(pady=10, padx=10)
+
+        button1 = tk.Button(self, text="Visit Page 1",
+                            command=lambda: controller.show_frame(pageOne))
+        button1.pack()
 
 
-app = grapher()
+class pageOne(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        label = tk.Label(self, text="Page One", font=LARGE_FONT)
+        label.pack(pady=10, padx=10)
+
+        button = tk.Button(self, text="Back To Home",
+                           command=lambda: controller.show_frame(startPage))
+        button.pack()
+
+
+app = Grapher()
 app.mainloop()
-
-
